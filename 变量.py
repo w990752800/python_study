de351e7957edaf48745b5617c4ae0d89d5d0a495
王宁飞 <ningfei.wang@jaka.com>
2025-05-28 15:54:34 +0800

    feat: variable learning

---
+++ 变量.py
@@ -0,0 +1,28 @@
+
+
+a = 100
+b = 200
+print(a + b)
+
+c = "xxxx"
+
+d = False
+e = 1.222
+
+print(type(a))
+
+print(type(c))
+
+print(d)
+print(type(e))
+
+print(int(d))
+
+# float int str bool 
+
+
+year = int(input('请输入年份'))
+is_leap = year % 4 == 0 and year % 100 != 0 or year % 400 == 4
+print(f'{year}年{is_leap}')
+
+print("xxx")
